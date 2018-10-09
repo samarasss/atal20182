3f582d1403e8e1de4c0ee7d166b9640eb371b72b
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2018-10-09 11:24:54 -0300

    Primeira atividade finalizada

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,29 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
+	quicksort(alist, 0, len(alist) - 1)
 	return alist
+	
+def partition(array, begin, end):
+    pivot = begin
+    for i in xrange(begin+1, end+1):
+        if array[i] >= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)
+    
+
