f559ff4e16954b4735260dec6a93c3e0c491162d
Samara Sampaio <samara.sampaio@ccc.ufcg.edu.br>
2018-10-23 21:46:26 -0300

    Atividade2

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -16,25 +16,33 @@ def retorna_matriculas_decrescente(alist):
 
 # Caso o valor não possa ser alcançado pela combinação de moedas o valor -1 é retornado Ex: valor = 11  moedas = {5, 10, 25}
 # Assuma que existe uma quantidade infinita de cada tipo de moeda
-def retorna_minimo_moedas(valor, tipos_moedas):
-	print valor, tipos_moedas
-	resultado = retorna_minimo_moedas_FB(tipos_moedas, valor)
-	if resultado == sys.maxint:
-		return -1
-	else:
-		return resultado
 
-def retorna_minimo_moedas_FB(tipos_moedas, valor):
-	if valor == 0:
+
+global resp
+resp = sys.maxint
+
+def retorna_minimo_moedas(valor, tipos_moedas):
+    global resp
+    resp = sys.maxint
+    print(valor, tipos_moedas)
+    tipos_moedas.sort(reverse = True)
+    resultado = retorna_minimo_moedas_FB(tipos_moedas, valor, 0)
+
+    if resultado == sys.maxint or resultado == None:
+        return -1
+    else:
+        return resultado
+
+def retorna_minimo_moedas_FB(tipos_moedas, valor,quantidadeMoedas):
+  	if valor == 0:
 		return 0
 	
 	resultado = sys.maxint
 	
-	for moeda in tipos_moedas:
+	contador = 0;
+	
+	for i in range(0, len(tipos_moedas)):
+		if (valor >= tipos_moedas[i] and quantidadeMoedas < resp):
+			return retorna_minimo_moedas_FB(tipos_moedas, (valor - tipos_moedas[i]), quantidadeMoedas + 1)
 		
-		if (moeda <= valor):
-			resultado = min(resultado, retorna_minimo_moedas_FB(tipos_moedas, valor - moeda) + 1) 
-		
-	return resultado	 
-
-
+	return resultado	
